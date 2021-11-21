4fd76a914bd4078aba0f216d161759f7b9680c8c
madortill <mador.till@gmail.com>
2021-11-21 17:41:44 +0200

    Update code.js

--- code.js
+++ code.js
@@ -9,7 +9,9 @@ var Arr_1 = [
   {
     // opening game question- page 1
     divName: ["opening-game-question", "general-opening-game"],
-    functions: ['pop_buttons($("#prev"), -1, "add")', 'pop_buttons($("#next"), +1, "add")', 'clearIntervalExplanation()', 'pop_openingGameQuestion()'],
+    functions: [""],
+    type: "content",
+    topic: 1
   },
 ];
 
@@ -29,14 +31,25 @@ var matrix = [[
   {
     // opening- page 0
     divName: ["opening"], // the last div contains the speech bubble
-    functions: ["pop_room_buttons()", 'pop_buttons($("#about"), 1)', "pop_calculateStrokeTextCSS(16)"] // array of functions that are needed to the page. If the functions contain the word "pop", it will happen only once and will be popped out of the array afterwards
+    functions: ['pop_room_buttons($("#room-button-1"))', 'pop_buttons($("#about-button"), 1)', "pop_calculateStrokeTextCSS(16)"], // array of functions that are needed to the page. If the functions contain the word "pop", it will happen only once and will be popped out of the array afterwards
+    type: ""
   },
   {
     // about- page 1
     divName: ["about"],
-    functions: [""]
+    functions: [""],
+    type: ""
   }
-], Arr_1, Arr_2, Arr_3, Arr_4]
+], Arr_1, Arr_2, Arr_3, Arr_4];
+
+// lesson map
+// what topic is the user currently learning
+var topic_counter = 1;
+// the distance between each circle in the lesson map (for the head movement)- different for each room 
+var topic_distance = 4;
+
+// life
+var nLife = 3;
 
 $(function() {
   // calls the opening page
@@ -65,19 +78,25 @@ function movePage() {
       }
     }
   }
+
+  // identify type
+  if (matrix[nRoom][nPage].type === "content") {
+    type_content();
+  }
 }
 
-// function that adds events listeners to buttons that affects the page's display- called only one time for each button
-function pop_room_buttons() {
-  $(".room-button").on("click", function() {
+// function that adds events listeners to room buttons that displays the chosen room- called only one time for each button
+function pop_room_buttons(button) {
+  button.on("click", function() {
     // hides last divs
     for (let i = 0; i < matrix[nRoom][nPage].divName.length; i++) {
       $("#" + matrix[nRoom][nPage].divName[i]).css("display", "none");
     }
     // changes room counter
-    nRoom = Number($(this).attr("id").slice(-1)); 
+    nRoom = Number(button.attr("id").slice(-1)); 
     // shows next page
-    movePage();     
+    movePage(); 
+    check_room();    
   })
 }
 
@@ -92,10 +111,12 @@ function pop_buttons(button, number) {
     // if the button is prev/next/about (ect), the number is added to page counter
     if (button.hasClass("move")){
       nPage = nPage + eval(number);
+      move_lessonMap(topic_distance * number);
     }
     // if the button is part of the lesson map, page counter is compared to the number 
     else if (button.hasClass("lesson-map")) {
       nPage = eval(number);
+      move_lessonMap(topic_distance * (topic_counter - number));
     }
     // shows next page
     movePage();     
@@ -119,6 +140,51 @@ function pop_calculateStrokeTextCSS(steps) {
   return css;
 }
 
-function check_type(){
+// function that is called every time going in to new room to start from stratch
+check_room = () => {
+  topic_counter = 1;
+  nLife = 3;
+  switch (nRoom) {
+    case 1:
+      topic_distance = 4;  
+      break;
+    case 2:
+      topic_distance = 2;  
+      break;
+    case 3:
+      topic_distance = 3;  
+      break;
+    case 4:
+      topic_distance = 2;  
+      break;
+    default:
+      topic_distance = 4; 
+  }
+}
+
+// moves ahami lesson map head- after moving topic and after every game
+//
+move_lessonMap = (distance) => {
+  // change lesson map head place
+  if (matrix[nRoom][nPage].type !== "") {
+    if (topic_counter !== matrix[nRoom][nPage].topic) {
+      topic_counter = matrix[nRoom][nPage].topic;
+      $("#topic-counter").animate({right: `${distance}vw`}, 1000);
+    }
+  }
+}
+
+// function that 
+type_content = () => {
 
 }
+
+switch_class = (object, prevClass, currClass) => {
+  object.removeClass(prevClass);
+  object.assClass(currClass);
+}
+
+
+
+
+	
