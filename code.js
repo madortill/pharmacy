e33cd8d32b553b5a74c433304e5a0f424c0ba6c8
madortill <mador.till@gmail.com>
2021-11-23 12:30:49 +0200

    Update code.js

--- code.js
+++ code.js
@@ -8,11 +8,18 @@ var nPage = 0;
 var Arr_1 = [
   {
     // opening game question- page 1
-    divName: ["opening-game-question", "general-opening-game"],
-    functions: [""],
+    divName: ["r1p1"],
+    functions: [`switch_class($("#back-button"), "visible", "hidden")`, `pop_buttons($("#next-button"), 1)`],
     type: "content",
     topic: 1
   },
+  {
+    // opening game question- page 1
+    divName: ["r1p2"],
+    functions: [`switch_class($("#back-button"), "hidden", "visible")`, `pop_buttons($("#back-button"), -1)`],
+    type: "content",
+    topic: 2
+  }
 ];
 
 // therapy room
@@ -31,14 +38,12 @@ var matrix = [[
   {
     // opening- page 0
     divName: ["opening"], // the last div contains the speech bubble
-    functions: ['pop_room_buttons($("#room-button-1"))', 'pop_buttons($("#about-button"), 1)', "pop_calculateStrokeTextCSS(16)"], // array of functions that are needed to the page. If the functions contain the word "pop", it will happen only once and will be popped out of the array afterwards
-    type: ""
+    functions: ['pop_room_buttons($("#room-button-1"))', 'pop_buttons($("#about-button"), 1)', "pop_calculateStrokeTextCSS(16)"] // array of functions that are needed to the page. If the functions contain the word "pop", it will happen only once and will be popped out of the array afterwards
   },
   {
     // about- page 1
     divName: ["about"],
-    functions: [""],
-    type: ""
+    functions: ['pop_buttons($("#back-about-button"), -1)']
   }
 ], Arr_1, Arr_2, Arr_3, Arr_4];
 
@@ -96,7 +101,10 @@ function pop_room_buttons(button) {
     nRoom = Number(button.attr("id").slice(-1)); 
     // shows next page
     movePage(); 
-    check_room();    
+    check_room(); 
+    // display room
+    $(`#room-${nRoom}`).css("display", "block");
+    setTimeout(toggle_room("hide"), 1000);   
   })
 }
 
@@ -107,21 +115,107 @@ function pop_buttons(button, number) {
     for (let i = 0; i < matrix[nRoom][nPage].divName.length; i++) {
       $("#" + matrix[nRoom][nPage].divName[i]).css("display", "none");
     }
+    // changing checkpoint in lesson map
+    if (matrix[nRoom][nPage].topic !== undefined) {
+      checkpoint(true);
+    }
     // changes page counter
     // if the button is prev/next/about (ect), the number is added to page counter
     if (button.hasClass("move")){
       nPage = nPage + eval(number);
-      move_lessonMap(topic_distance * number);
+      // lessom map movememt (ahami head)
+        // if the topic changes whem moving page (there are pages with the same topic)
+        // after a game the topic is equal to the content topic and there is no need to change
+        if ((matrix[nRoom][nPage].topic !== undefined) && (topic_counter !== matrix[nRoom][nPage].topic)) {
+          move_lessonMap(topic_distance * number);
+        }
     }
     // if the button is part of the lesson map, page counter is compared to the number 
-    else if (button.hasClass("lesson-map")) {
+    else if (button.hasClass("topic")) {
       nPage = eval(number);
-      move_lessonMap(topic_distance * (topic_counter - number));
+      // lessom map movememt (ahami head)
+        move_lessonMap(topic_distance * (matrix[nRoom][nPage].topic - topic_counter));
     }
     // shows next page
     movePage();     
   })
 }
+
+// function that is called every time going in to new room to start from stratch
+check_room = () => {
+  topic_counter = 1;
+  nLife = 3;
+  switch (nRoom) {
+    case 1:
+      topic_distance = 13;  
+      break;
+    case 2:
+      topic_distance = 2;  
+      break;
+    case 3:
+      topic_distance = 3;  
+      break;
+    case 4:
+      topic_distance = 2;  
+      break;
+    default:
+      topic_distance = 4; 
+  }
+  $("#topic-counter").css("right", "-63.5vw");
+  // controls
+  switch_class($("#controls"), "none", "flex");
+  switch_class($(`#lesson-map-${nRoom}`), "none", "flex");
+}
+
+
+// colors checkpoint if needed
+checkpoint = (condition) => {
+  let curr_checkpoint = $(`#lesson-map-${nRoom} .topic-${topic_counter}`);
+  // if the checkpoint haven't been changed
+  if (curr_checkpoint.css("background-image").includes("normal")) {
+    // if this is a content page or the user succeded in a game
+    if (condition) {
+      curr_checkpoint.css("background-image", `url("assets/media/2content/checkpoint_right.svg")`);
+    }
+    // the user lost the game
+    else {
+      curr_checkpoint.css("background-image", `url("assets/media/2content/checkpoint_wrong.svg")`);
+    }
+    // the checkpoint is clickable
+    pop_buttons(curr_checkpoint, nPage);
+    curr_checkpoint.addClass("button");
+  }
+}
+
+// moves ahami lesson map head- after moving topic and after every game
+move_lessonMap = (distance) => {
+  // change lesson map head place
+  $("#topic-counter").animate({right: `+=${distance}vw`}, 1000);
+  // update topic counter
+  topic_counter = matrix[nRoom][nPage].topic;
+}
+
+// display/hides room image (every room start and with the room button)
+toggle_room = (action)  => {
+  if (action === "show") {
+    $(`#room-${nRoom}`).css("display","block").animate({opacity: `1`}, 1000);
+  } else if (action === "hide") {
+    $(`#room-${nRoom}`).animate({opacity: `0`}, 1000).css("display","none");
+  }
+}
+
+// function that 
+type_content = () => {
+
+}
+
+// function that switches classes
+switch_class = (object, prevClass, currClass) => {
+  if (object.hasClass(prevClass)) {
+    object.removeClass(prevClass);
+    object.addClass(currClass);
+  }
+}	
 
 // text css opening
 function pop_calculateStrokeTextCSS(steps) {
@@ -139,47 +233,3 @@ function pop_calculateStrokeTextCSS(steps) {
   }
   return css;
 }
-
-// function that is called every time going in to new room to start from stratch
-check_room = () => {
-  topic_counter = 1;
-  nLife = 3;
-  switch (nRoom) {
-    case 1:
-      topic_distance = 4;  
-      break;
-    case 2:
-      topic_distance = 2;  
-      break;
-    case 3:
-      topic_distance = 3;  
-      break;
-    case 4:
-      topic_distance = 2;  
-      break;
-    default:
-      topic_distance = 4; 
-  }
-}
-
-// moves ahami lesson map head- after moving topic and after every game
-//
-move_lessonMap = (distance) => {
-  // change lesson map head place
-  if (matrix[nRoom][nPage].type !== "") {
-    if (topic_counter !== matrix[nRoom][nPage].topic) {
-      topic_counter = matrix[nRoom][nPage].topic;
-      $("#topic-counter").animate({right: `${distance}vw`}, 1000);
-    }
-  }
-}
-
-// function that 
-type_content = () => {
-
-}
-
-switch_class = (object, prevClass, currClass) => {
-  object.removeClass(prevClass);
-  object.assClass(currClass);
-}	
