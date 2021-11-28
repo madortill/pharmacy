f6816e5211adf3f8e8fc43a87c600ad330a8b4df
madortill <mador.till@gmail.com>
2021-11-28 12:52:02 +0200

    Update games_general.js

--- games_general.js
+++ games_general.js
@@ -3,7 +3,7 @@
 type_game = () => {
     // hide controls
     switch_class($("#controls"), "flex" ,"none");
-    switch_class($(`#lesson-map-${nRoom}`),"flex" ,"none");
+    switch_class($(`#lesson-map-${nRoom}`), "flex" ,"none");
   
     // display timer
     switch_class($("#timer"), "none", "block");
@@ -12,24 +12,32 @@ type_game = () => {
   }
 
 endingGame = (condition) => {
-    hidePage(condition);
+    hidePage();
+    checkpoint(condition);
+    // hide timer
+    switch_class($("#timer"), "block", "none");
     // display end-game general page
-    $(`#ending-game`).css("display", "block");  
-    // moving one step in less point
+    $(`#ending-game`).css("display", "block");
+    switch_class($("#spinning-flex"), "none", "flex");
+    // moving one step in lesson map
+    switch_class($("#controls"),"none", "flex" );
+    switch_class($(`#lesson-map-${nRoom}`), "none", "flex");  
     move_lessonMap(topic_distance);
     // removing game from the array
-    ArrPages.splice(nPage + exerPage , 1);
+    matrix[nRoom].splice(nPage , 1);
     // shows next page
     setTimeout(() => {
         movePage();
+        // hide end-game general page
         $(`#ending-game`).css("display", "none");
-    }, 3500);
+        switch_class($("#spinning-flex"), "flex", "none");
+    }, 5000);
 }
 
   // time out- the game is over
 pop_timeEnds = () => {
     // event listener for ending timer animation
-    document.querySelector("#time-timeline").addEventListener("animationed", () => {
+    document.querySelector("#time-timeline").addEventListener("animationend", () => {
         endingGame(false);
-    })
+    });
 }
