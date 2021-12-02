c581f49a6c2f84da7cedc5ca711d3eb66a0bc1bb
madortill <mador.till@gmail.com>
2021-12-02 09:53:49 +0200

    Update room_1_games.js

--- room_1_games.js
+++ room_1_games.js
@@ -1,14 +1,11 @@
 // called to add to each sign event listener to click_identify
-// in order the function will work the items need to have the class "state" and "item"
+// in order the function will work the items need to have the class "item"
 // r1p3
 pop_sign_click = () => {
     // add event listener for each item
-    for (let i = 1; i <= $(`#${matrix[nRoom][nPage].divName} .item`).length ; i++) {
-        let item =  $(`#${matrix[nRoom][nPage].divName} .item.data-num-${i}`);
-        item.on("click", () => {
-            click_identify(item);
-        }); 
-    } 
+    $(`#${matrix[nRoom][nPage].divName} .item`).on("click", () => {
+        click_identify($(this));
+    }); 
 }
 
 // the parameter is the clicked correct sign
