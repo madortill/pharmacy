4bec7b5e4a6e889dfda0ecee1c89983b778771a0
madortill <mador.till@gmail.com>
2022-01-10 09:38:58 +0200

    Update code.js

--- code.js
+++ code.js
@@ -6,54 +6,54 @@ var nPage = 0;
 
 // waiting room
 var Arr_1 = [
-  // {
-  //   // opening game question- page 1
-  //   divName: ["r1p1"],
-  //   functions: [`switch_class($("#back-button"), "visible", "hidden")`, `pop_buttons($("#next-button"), 1)`, "pop_watch_room_button()"],
-  //   type: "content",
-  //   topic: 1
-  // },
-  // {
-  //   // page 2
-  //   divName: ["r1p2"],
-  //   functions: [`switch_class($("#back-button"), "hidden", "visible")`, `pop_buttons($("#back-button"), -1)`],
-  //   type: "content",
-  //   topic: 2
-  // },
-  // {
-  //   // first game- page 3
-  //   divName: ["r1p3"],
-  //   functions: ["pop_timeEnds()", "pop_sign_click()", "pop_hover_down()"],
-  //   type: "game",
-  //   timer: "10s",
-  //   feedback: {
-  //     correct: "array",
-  //     incorrect: "array"
-  //   },
-  //   instructions_feedback: {
-  //     correct: "אתם שולטים על השלטים!",
-  //     incorrect: "חבל שתליית השלטים תלוייה בכם..."
-  //   }
-  // },
-  // {
-  //   // page 4
-  //   divName: ["r1p4"],
-  //   functions: [""],
-  //   type: "content",
-  //   topic: 3
-  // },
-  // {
-  //   // page 5
-  //   divName: ["r1p5"],
-  //   functions: [""],
-  //   type: "content"
-  // },
-  // {
-  //   // page 6
-  //   divName: ["r1p6"],
-  //   functions: [""],
-  //   type: "content"
-  // },
+  {
+    // opening game question- page 1
+    divName: ["r1p1"],
+    functions: [`switch_class($("#back-button"), "visible", "hidden")`, `pop_buttons($("#next-button"), 1)`, "pop_watch_room_button()"],
+    type: "content",
+    topic: 1
+  },
+  {
+    // page 2
+    divName: ["r1p2"],
+    functions: [`switch_class($("#back-button"), "hidden", "visible")`, `pop_buttons($("#back-button"), -1)`],
+    type: "content",
+    topic: 2
+  },
+  {
+    // first game- page 3
+    divName: ["r1p3"],
+    functions: ["pop_timeEnds()", "pop_sign_click()", "pop_hover_down()"],
+    type: "game",
+    timer: "10s",
+    feedback: {
+      correct: "array",
+      incorrect: "array"
+    },
+    instructions_feedback: {
+      correct: "אתם שולטים על השלטים!",
+      incorrect: "חבל שתליית השלטים תלוייה בכם..."
+    }
+  },
+  {
+    // page 4
+    divName: ["r1p4"],
+    functions: [""],
+    type: "content",
+    topic: 3
+  },
+  {
+    // page 5
+    divName: ["r1p5"],
+    functions: [""],
+    type: "content"
+  },
+  {
+    // page 6
+    divName: ["r1p6"],
+    functions: [""],
+    type: "content"
+  },
   {
     // second game- page 7
     divName: ["r1p7"],
@@ -67,6 +67,19 @@ var Arr_1 = [
       correct: "מזל שהמשימה הייתה תלוייה בכם",
       incorrect: "יא חסרי מודעות..."
     }
+  },
+  {
+    // page 8
+    divName: ["r1p8"],
+    functions: [""],
+    type: "content",
+    topic: 4
+  },
+  {
+    // page 9
+    divName: ["r1p9"],
+    functions: [""],
+    type: "content"
   },
 ];
 
@@ -201,7 +214,7 @@ check_room = () => {
   nLife = 3;
   switch (nRoom) {
     case 1:
-      topic_distance = 13;  
+      topic_distance = 15;  
       break;
     case 2:
       topic_distance = 2;  
